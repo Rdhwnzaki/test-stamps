ba65f582b82c105283797c300b181e3cc81a2544
Rdhwnzaki <zaki85.rmz@gmail.com>
2023-05-29 12:23:56 +0700

    finish

---
+++ soal1.js
@@ -0,0 +1,18 @@
+const apaBole = () => {
+  let output = "";
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      output += "ApaBole ";
+    } else if (i % 3 == 0) {
+      output += "Apa ";
+    } else if (i % 5 == 0) {
+      output += "Bole ";
+    } else {
+      output += i + " ";
+    }
+  }
+
+  console.log(output);
+};
+
+apaBole();
--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import Moment from "react-moment";
 
 function App() {
+  const [day1, setDay1] = useState([]);
+  const [weatherDay1, setWeatherDay1] = useState([]);
+  const [day2, setDay2] = useState([]);
+  const [weatherDay2, setWeatherDay2] = useState([]);
+  const [day3, setDay3] = useState([]);
+  const [weatherDay3, setWeatherDay3] = useState([]);
+  const [day4, setDay4] = useState([]);
+  const [weatherDay4, setWeatherDay4] = useState([]);
+  const [day5, setDay5] = useState([]);
+  const [weatherDay5, setWeatherDay5] = useState([]);
+  const getData = async () => {
+    axios
+      .get(
+        "http://api.openweathermap.org/data/2.5/forecast?q=Jakarta&appid=5b02411f2d86893aaf3fc1ef179976a8&units=metric"
+      )
+      .then((res) => {
+        console.log(res.data.list);
+        setDay1(res.data.list[8].dt_txt);
+        setWeatherDay1(res.data.list[8].main.temp);
+        setDay2(res.data.list[16].dt_txt);
+        setWeatherDay2(res.data.list[16].main.temp);
+        setDay3(res.data.list[24].dt_txt);
+        setWeatherDay3(res.data.list[24].main.temp);
+        setDay4(res.data.list[32].dt_txt);
+        setWeatherDay4(res.data.list[32].main.temp);
+        setDay5(res.data.list[39].dt_txt);
+        setWeatherDay5(res.data.list[39].main.temp);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h4>Weather Forecast :</h4>
+      <h5>
+        <Moment format="LLLL">{day1}</Moment> : {weatherDay1}°C
+      </h5>
+      <h5>
+        <Moment format="LLLL">{day2}</Moment> : {weatherDay2}°C
+      </h5>
+      <h5>
+        <Moment format="LLLL">{day3}</Moment> : {weatherDay3}°C
+      </h5>
+      <h5>
+        <Moment format="LLLL">{day4}</Moment> : {weatherDay4}°C
+      </h5>
+      <h5>
+        <Moment format="LLLL">{day5}</Moment> : {weatherDay5}°C
+      </h5>
+    </>
   );
 }
 
